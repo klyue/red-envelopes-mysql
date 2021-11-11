a3a6c1ff24990b50c1c0997c6a42ff019edf6b67
yangxc <yangxc@sucsoft.com>
2021-11-11 23:04:19 +0800

    First commit

---
+++ src/main/java/com/group6/redenvelopesmysql/RedEnvelopesMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.group6.redenvelopesmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedEnvelopesMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedEnvelopesMysqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group6/redenvelopesmysql/RedEnvelopesMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group6.redenvelopesmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedEnvelopesMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
