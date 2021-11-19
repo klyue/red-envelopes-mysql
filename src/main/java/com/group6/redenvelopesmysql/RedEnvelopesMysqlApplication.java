a109cffdd08fa1a468a1f616bef0ff3bf765f528
yangxc <yangxc@sucsoft.com>
2021-11-19 17:31:13 +0800

    Fix transaction not opened bug

--- src/main/java/com/group6/redenvelopesmysql/RedEnvelopesMysqlApplication.java
+++ src/main/java/com/group6/redenvelopesmysql/RedEnvelopesMysqlApplication.java
@@ -3,7 +3,9 @@ package com.group6.redenvelopesmysql;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @EnableConfigurationProperties
 @SpringBootApplication
 public class RedEnvelopesMysqlApplication {
